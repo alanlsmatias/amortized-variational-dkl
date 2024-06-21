77ac72e6f96fe03ffcb0a609b78c16704e113858
Alan Matias <matiasalsm@gmail.com>
2024-06-21 11:58:15 -0300

    add gpinfuser source code

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    'numpy',
+    'scipy',
+    'torch',
+    'torchaudio',
+    'torchvision',
+    'torchmetrics',
+    'torch_geometric',
+    'gpytorch',
+    'pandas',
+    'pyarrow',
+    'scikit-learn',
+    'jupyterlab',
+    'matplotlib',
+    'seaborn'
+]
+
+setup(
+    name='gpinfuser',
+    version='0.0.1',
+    author='Alan Matias',
+    author_email='matiasalsm@gmail.com',
+    description='Infusing GPs with Deep Kernels and Amortized Inference',
+    install_requires=requirements,
+    package_dir={'': 'src'},
+    packages=find_packages('src')
+)
